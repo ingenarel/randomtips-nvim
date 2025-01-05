cc0328e9389de1e708197b93b3a0189f40cf9571
ingenarel <ingenarelitems@gmail.com>
2025-01-06 01:21:48 +0600

    added more witcher 3 qoutes

--- lua/randomtips/witcher3.lua
+++ lua/randomtips/witcher3.lua
@@ -1,3 +1,26 @@
 return {
     { "Winds howling..." },
+    {
+        "Hatred And Prejudice Will Never Be Eradicated. And Witch Hunts Will Never Be About Witches. To Have A Scapegoat - That’s The Key.",
+    },
+    { "Wanna hear a limerick? Lambert, Lambert.. What a prick" },
+    { "You Don't Need Mutations To Strip Men Of Their Humanity. I've Seen Plenty Of Examples." },
+    { "Time Eats Away At Memories, Distorts Them. Sometimes We Only Remember The Good... Sometimes Only The Bad." },
+    {
+        "Evil is evil. Lesser, greater, middling, makes no difference. The degree is arbitrary. The definitions blurred.",
+        "If I'm to choose between one evil and another, I'd rather not choose at all.",
+    },
+    {
+        "Witchers were made to kill monsters. It doesn't matter who posted the notice, the coin has to be right, that's all. ",
+    },
+    { "Know when a legend becomes a prophecy? When it gains believers." },
+    {
+        "Finish all your business before you die. Bid loved ones farewell. Write your will. Apologize to those you’ve wronged.",
+        "Otherwise, you’ll never truly leave this world.",
+    },
+    { "No witcher ever died in his bed" },
+    { "If you're about to be hanged, ask for a glass of water. Anything can happen before they fetch it." },
+    { "Only the innocent burn in Novigrad" },
+    { "When the time of the White Frost comes, do not eat the yellow snow." },
+    { "THAT BITCH HEXED ME!!!" },
 }
